7af3485bcb9e2561114856653b231f41ba77c35c
1634theAM <1634theam@gmail.com>
2025-10-01 01:25:21 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import KleviBot from "./components/KleviBot";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient();
 
